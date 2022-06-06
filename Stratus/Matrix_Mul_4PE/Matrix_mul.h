91a6d685d37fdc694073e59a5253d093a3a3a737
ku1248 <101209771+ku1248@users.noreply.github.com>
2022-06-06 15:52:26 +0800

    Add files via upload

---
+++ Stratus/Matrix_Mul_4PE/Matrix_mul.h
@@ -0,0 +1,34 @@
+#ifndef SOBEL_FILTER_H_
+#define SOBEL_FILTER_H_
+#include <systemc>
+using namespace sc_core;
+
+#ifndef NATIVE_SYSTEMC
+#include <cynw_p2p.h>
+#endif
+
+#include "matrix_mul_def.h"
+#define V 6
+class Matrix_mul: public sc_module
+{
+public:
+	sc_in_clk i_clk;
+	sc_in < bool >  i_rst;
+#ifndef NATIVE_SYSTEMC
+	cynw_p2p< sc_dt::sc_uint<8> >::in i_map;
+	cynw_p2p< sc_dt::sc_uint<32> >::out o_result;
+#else
+	sc_fifo_in< sc_dt::sc_uint<8> > i_map;
+	sc_fifo_out< sc_dt::sc_uint<32> > o_result;
+#endif
+
+	SC_HAS_PROCESS( Matrix_mul );
+	Matrix_mul( sc_module_name n );
+	~Matrix_mul();
+private:
+	sc_dt::sc_uint<8> B[64];
+	sc_dt::sc_uint<8> A[8];
+
+	void do_matrix_mul();
+};
+#endif
